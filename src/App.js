f2551873fc15ad0bf781bb4456c2d049c6e5f97d
Christopher Rumler <crumler@dminc.com>
2021-04-11 08:11:00 -0400

    added useState code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import ToDoList from './components/ToDoList';
 import AddToDo from './components/AddToDo';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 1, text: 'Wash Dishes', done: false },
     { id: 2, text: 'Do Laundry', done: false },
     { id: 3, text: 'Take Shower', done: false },
-  ];
+  ]);
 
   return (
     <div className="App">
--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -2,11 +2,17 @@
 const AddToDo = () => {
     function handleToDo(event) {
         event.preventDefault();
+        const text = event.target.elements.addToDo.value;
+        const todo = {
+            id: 4,
+            text,
+            done: false
+        };
     };
 
     return(
         <form onSubmit={handleToDo}>
-            <input placeholder="Add To-Do Item" />
+            <input placeholder="Add To-Do Item" name="addToDo" />
             <button type="submit">Submit</button>
         </form>
     )
